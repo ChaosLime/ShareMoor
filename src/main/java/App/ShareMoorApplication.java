b49f407fefa6cb29b59aa8f0910cff966efc16a8
Nicholas Saunders <nicholas3000@sbcglobal.net>
2020-08-31 23:08:02 -0700

    init commit, generic blank springboot, java 14

---
+++ src/main/java/App/ShareMoorApplication.java
@@ -0,0 +1,13 @@
+package App;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShareMoorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShareMoorApplication.class, args);
+	}
+
+}
